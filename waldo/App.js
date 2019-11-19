5c611c3c74201157239117365f46b533d43b5797
AFisker <57772647+AFisker@users.noreply.github.com>
2019-11-19 21:20:24 +0100

    add background image

--- waldo/App.js
+++ waldo/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, } from 'react-native';
+
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <ImageBackground source={require('./assets/bike.jpg')} style={{width: '100%', height: '100%'}}>
+      </ImageBackground>
     </View>
   );
 }
