93fb73f32685406c96ad5a0f6cfa3b2867355035
macbook <macbook@macbooks-MacBook-Pro-2.local>
2019-11-28 16:47:24 +0100

    Tilføj kamera

--- waldo/screens/camera.js
+++ waldo/screens/camera.js
@@ -1,13 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import { FontAwesome, Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function CameraScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+
+<FontAwesome
+  name="camera"
+  style={{ color: "#fff", fontSize: 40}}
+/>
+
+export default class CameraScreen extends React.Component {
+  
+  state = {
+    hasPermission: null,
+    type: Camera.Constants.Type.back,
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasPermission: status === 'granted' });
+  }
+
+  render(){
+    const { hasPermission } = this.state
+    if (hasPermission === null) {
+      return <View />;
+    } else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+         
+           
+            <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:20}}>
+            <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',                  
+                }}>
+                <Ionicons
+                    name="ios-photos"
+                    style={{ color: "#fff", fontSize: 40, position: 'absolute', bottom: 0, left: 0,}}
+                  
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}>
+                <FontAwesome
+                    name="camera"
+                    style={{ color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}>
+                <MaterialCommunityIcons
+                    name="camera-switch"
+                    style={{ color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+            </Camera>
+        </View>
+        
+      );
+    }
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
