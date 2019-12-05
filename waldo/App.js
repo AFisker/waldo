874f902fc782fa6c987dcec32617476d0693eaa4
Rasmus <52420892+Rallemor@users.noreply.github.com>
2019-12-05 16:37:07 +0100

    added image site

--- waldo/App.js
+++ waldo/App.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './screens/home';
 import CameraScreen from './screens/camera';
 import MapScreen from './screens/map';
+import Showimage from './showimage/image';
 
 
 const AppNavigator = createStackNavigator(
@@ -14,7 +15,8 @@ const AppNavigator = createStackNavigator(
   {
     homescreen: HomeScreen,
     camerascreen: CameraScreen,
-    mapscreen: MapScreen
+    mapscreen: MapScreen,
+    showimage: Showimage,
   },
   {
     initialRouteName: 'homescreen',
